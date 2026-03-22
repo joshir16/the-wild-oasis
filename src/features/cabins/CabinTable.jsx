5115feb216fcfb419dd2886e36e4b82028e12097
Rishabh <joshir838@gmail.com>
2026-03-22 07:48:59 +0530

    menu created

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabin } from "./useCabins";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 const TableHeader = styled.header`
   display: grid;
@@ -26,20 +27,23 @@ function CabinTable() {
   if (error) return <p>{error}</p>;
 
   return (
-    <Table columns={"0.6fr 1.8fr 2.2fr 1fr 1fr 1fr"}>
-      <Table.Header>
-        <div></div>
-        <div>Cabin</div>
-        <div>Capacity</div>
-        <div>Price</div>
-        <div>Discount</div>
-        <div></div>
-      </Table.Header>
+    <Menus>
+      <Table columns={"0.6fr 1.8fr 2.2fr 1fr 1fr 1fr"}>
+        <Table.Header>
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
 
-      {cabins.map((cabin) => (
-        <CabinRow cabin={cabin} key={cabin.name} />
-      ))}
-    </Table>
+        <Table.Body
+          data={cabins}
+          render={(cabin) => <CabinRow cabin={cabin} key={cabin.name} />}
+        />
+      </Table>
+    </Menus>
   );
 }
 
