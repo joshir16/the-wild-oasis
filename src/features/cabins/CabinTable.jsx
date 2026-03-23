7f2adcba48482a7cede625daec6314428dbf83d6
Rishabh <joshir838@gmail.com>
2026-03-23 06:21:45 +0530

    filter and sort components

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,30 +1,32 @@
-import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabin } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   const { isPending, cabins, error } = useCabin();
+  const [searchParams] = useSearchParams();
 
   if (isPending) return <Spinner />;
   if (error) return <p>{error}</p>;
+
+  const filterValue = searchParams.get("discount") || "all";
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier,
+  );
 
   return (
     <Menus>
@@ -39,7 +41,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.name} />}
         />
       </Table>
---
+++ src/ui/SortBy.jsx
@@ -0,0 +1,23 @@
+import { useSearchParams } from "react-router-dom";
+import Select from "./Select";
+
+function SortBy({ options }) {
+  const [searcParams, setSearchParams] = useSearchParams();
+  const sortBy = searcParams.get("sortBy") || "";
+
+  function handleChange(e) {
+    searcParams.set("sortBy", e.target.value);
+    setSearchParams(searcParams);
+  }
+
+  return (
+    <Select
+      options={options}
+      value={sortBy}
+      type={"white"}
+      onChange={handleChange}
+    />
+  );
+}
+
+export default SortBy;
